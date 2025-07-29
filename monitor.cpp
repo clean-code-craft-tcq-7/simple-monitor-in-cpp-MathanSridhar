9c3e58e67fda4b4b8c12f9b4ea6084b108058af8
MathanSridhar <91043184+MathanSridhar@users.noreply.github.com>
2025-07-29 10:44:00 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -3,36 +3,43 @@
 #include <thread>
 #include <chrono>
 #include <iostream>
+#include <string>
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
-int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if (temperature > 102 || temperature < 95) {
-    cout << "Temperature is critical!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  } else if (pulseRate < 60 || pulseRate > 100) {
-    cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  } else if (spo2 < 90) {
-    cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
+void displayVitalAlert(const std::string& message) {
+  cout << message;
+  for (int i = 0; i < 6; i++) {
+    cout << "\r* " << flush;
+    sleep_for(seconds(1));
+    cout << "\r *" << flush;
+    sleep_for(seconds(1));
   }
-  return 1;
+}
+
+bool checkTemperature(float temp) {
+    if (temp < 95 || temp > 102) {
+        showAlert("Temperature is critical!");
+        return false;
+    }
+    return true;
+}
+
+bool checkPulse(float pulse) {
+    if (pulse < 60 || pulse > 100) {
+        showAlert("Pulse Rate is critical!");
+        return false;
+    }
+    return true;
+}
+
+bool checkSpo2(float spo2) {
+    if (spo2 < 90) {
+        showAlert("Oxygen Saturation is critical!");
+        return false;
+    }
+    return true;
+}
+
+bool vitalsAreNormal(float temp, float pulse, float spo2) {
+    return checkTemperature(temp) && checkPulse(pulse) && checkSpo2(spo2);
 }
